9c65623875b60c3d96f7e71785c9a0d32be2f73a
jakeJeong31 <cheerjake@gmail.com>
2022-08-04 12:36:08 +0900

    dynamic pages by EJS

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // index.js
-﻿
-﻿var express = require('express');
+
+var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public')); // 1
+app.set('view engine','ejs'); // 1
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function(req,res){ // 2
+  res.render('hello', {name:req.query.nameQuery});
+});
+
+app.get('/hello/:nameParam', function(req,res){ // 3
+  res.render('hello', {name:req.params.nameParam});
+});
 
 var port = 3000;
 app.listen(port, function(){
