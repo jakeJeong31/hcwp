3389b48c24e8ecb1643be29f6829eedd947bb1ab
jakeJeong31 <cheerjake@gmail.com>
2022-08-13 20:49:38 +0900

    simple structure set and to ready to upload on AWS

--- routes/home.js
+++ routes/home.js
@@ -16,7 +16,6 @@ router.get('/terraform', function(req, res){
   res.render('home/terraform');
 });
 router.get('/consul', function(req, res){
-  //exec('curl http://192.168.50.248:8500/v1/health/service/consul',(err,stdout,stderr) => console.log(stdout));
   res.render('home/consul');
 });
 router.get('/vault', function(req, res){
@@ -29,29 +28,34 @@ router.get('/nomad', function(req, res){
 router.post('/ajax_call_api', function(req, res){
   console.log(req.body);  // param
   var api_flag = req.body.api_flag;
-  var rData = "";
-  if(api_flag=="cslSrvcSttsInfo"){
-    api_addr = "curl http://192.168.50.248:8500/v1/health/node/SbisLinux-Consul2";
-  } else if(api_flag=="cslSrvcNmInfo"){
-    api_addr = "curl http://192.168.50.248:8500/v1/catalog/node/SbisLinux-Consul2";
-  } else if(api_flag=="nmdVerChkBtn"){
-    api_addr = "curl http://192.168.50.235:4646/v1/jobs";
-  } else if (api_flag=='nmdAgtSvrPtchInfo') {
-    api_addr = "curl http://192.168.50.235:4646/v1/node/4db6c2d8-4849-ea9f-abb1-571ea1b86050";
-  } else if (api_flag=='nmdAgtClntPtchInfo') {
-    api_addr = "curl http://192.168.50.235:4646/v1/node/b3d7055c-0ddc-3b77-1c63-6909c217f2d2";
+  var api_exec = "";
+
+  if(api_flag=="cslSrvcSttsInfo"){ // consul
+    api_addr = "curl -s http://192.168.50.248:8500/v1/health/node/SbisLinux-Consul2";
+  } else if (api_flag=="cslSrvcSttsInfo_2nd"){
+    api_addr = "curl -s http://192.168.50.248:8500/v1/health/state/passing";
+  } else if (api_flag=="cslSrvcNmInfo"){
+    api_addr = "curl -s http://192.168.50.248:8500/v1/catalog/node/SbisLinux-Consul2";
+  } else if (api_flag=='nmdAgtSvrPtchInfo') { // nomad
+    api_addr = "curl -s http://192.168.50.235:4646/v1/node/4db6c2d8-4849-ea9f-abb1-571ea1b86050";
+  } else if (api_flag=='nmdJobNmInfo') {
+    api_addr = "curl -s http://192.168.50.235:4646/v1/job/example";
+  } else if (api_flag=='trrfrmRun') {
+    api_addr = "curl -s 'Authorization: Bearer $TEAM_TOKEN' https://app.terraform.io/api/v2/runs/run-UQJ55vGbhnhdyzXg";
+  } else if (api_flag=='trrfrmWorkspace') {
+    api_addr = "curl -s 'Authorization: Bearer $TEAM_TOKEN' https://app.terraform.io/api/v2/organizations/Insideinfo/workspaces";
   } else {
     console.log("api_flag is null");
   }
 
-  rData = exec(api_addr, function (error, stdout, stderr){
+  api_exec = exec(api_addr, function (error, stdout, stderr){
     console.log('stdout: '+stdout);
     console.log('stderr: '+stderr);
     if(error != null){
       console.log('exec error: '+error);
     }
-    var obj = JSON.parse(stdout);
-    res.json({rData:obj});
+    var rData = JSON.parse(stdout);
+    res.json({rData:rData});
   });
 
 });
