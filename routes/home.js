571702162e1c3d64e88971f8588b8a7702457df5
jakeJeong31 <cheerjake@gmail.com>
2022-09-09 14:37:12 +0900

    vault api function

--- routes/home.js
+++ routes/home.js
@@ -28,7 +28,7 @@ router.get('/consul', function(req, res){
   res.render('home/consul');
 });
 router.get('/vault', function(req, res){
-  res.render('home/vault');
+  res.render('home/vault',{gData:myModule});
 });
 router.get('/nomad', function(req, res){
   res.render('home/nomad');
@@ -37,6 +37,8 @@ router.get('/nomad', function(req, res){
 router.post('/ajax_call_api', function(req, res){
   console.log(req.body);  // param
   var api_flag = req.body.api_flag;
+  var vault_ip_addr = req.body.vault_ip_addr;
+  var vault_clientToken = req.body.client_token;
   var api_exec = "";
 
   if(api_flag=="cslSrvcSttsInfo"){ // consul
@@ -53,6 +55,12 @@ router.post('/ajax_call_api', function(req, res){
     api_addr = 'curl -s --header "Authorization: Bearer $TEAM_TOKEN" https://app.terraform.io/api/v2/runs/run-UQJ55vGbhnhdyzXg';
   } else if (api_flag=='trrfrmWorkspace') {
     api_addr = 'curl -s --header "Authorization: Bearer '+myModule.TERRAFORM_INFO[0].team_token+'" https://app.terraform.io/api/v2/organizations/Insideinfo/workspaces';
+  } else if (api_flag=='vltAccInfo') {
+    api_addr = 'curl --request POST --data \"{""password"": ""'+myModule.VAULT_INFO[0].usps_password+'""}\" http://'+vault_ip_addr+":"+myModule.VAULT_INFO[0].port+'/v1/auth/userpass/login/test';
+  } else if (api_flag=='vltDbCredInfo') {
+    api_addr = 'curl --header "X-Vault-Token:'+vault_clientToken+'" http://'+vault_ip_addr+":"+myModule.VAULT_INFO[0].port+'/v1/database/creds/workshop-app';
+  } else if (api_flag=='vltOtpCredInfo') {
+    api_addr = 'curl --header "X-Vault-Token:'+vault_clientToken+'" --request POST --data \"{""ip"": ""'+vault_ip_addr+'""}\"  http://'+vault_ip_addr+":"+myModule.VAULT_INFO[0].port+'/v1/ssh/creds/otp_key_role';
   } else {
     console.log("api_flag is null");
   }
